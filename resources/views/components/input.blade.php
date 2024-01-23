36be55fde3d27d4840bf56ddfb9e761fe0b72166
maggie ossman <mrsmargareto@gmail.com>
2024-01-23 10:16:34 -0600

    Style - Updated Tailwind Classes

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-violet-500 focus:ring-violet-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-violet-300 focus:border-violet-500 bg-violet-50 focus:ring-violet-500 rounded-md shadow-sm']) !!}>
